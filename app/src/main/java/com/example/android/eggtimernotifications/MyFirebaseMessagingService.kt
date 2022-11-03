721f99b83f8928a5c042f1374941a494feaa2e66
Lavrikov Pavel <laurus.p.a.work@gmail.com>
2022-11-03 15:38:17 +0400

    6. Task: Sending data with FCM

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -25,6 +25,14 @@ import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
+    private val notificationManager: NotificationManager by lazy {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        )
+        requireNotNull(notificationManager)
+    }
+
     /**
      * Called when message is received.
      *
@@ -35,12 +43,14 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage.from}")
 
-        // TODO Step 3.5 check messages for data
-        // Check if message contains a data payload.
+        Log.d(TAG, "Remote message payload: ${remoteMessage.data}")
 
+        val receivedNotificationData = remoteMessage.notification
+        if (receivedNotificationData != null) {
+            Log.d(TAG, "Received notification data: $receivedNotificationData")
+            sendNotification(messageBody = receivedNotificationData.body ?: "")
+        }
 
-        // TODO Step 3.6 check messages for notification and call sendNotification
-        // Check if message contains a notification payload.
 
     }
     // [END receive_message]
@@ -65,7 +75,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * @param messageBody FCM message body received.
      */
     private fun sendNotification(messageBody: String) {
-        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
         notificationManager.sendNotification(messageBody, applicationContext)
     }
 
